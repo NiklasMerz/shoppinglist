58b4dab0360f1ec95fa17392334b04cc0bbe8f4f
Niklas Merz <niklasmerz@linux.com>
2021-05-16 03:30:30 +0200

    Try another env setup

--- ShoppingListPlatform/asgi.py
+++ ShoppingListPlatform/asgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ShoppingListPlatform.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from turbo.consumers import TurboStreamsConsumer
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ShoppingListPlatform.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ShoppingListPlatform.settings")
 
 application = ProtocolTypeRouter(
     {"http": get_asgi_application(), "websocket": TurboStreamsConsumer.as_asgi()}
--- ShoppingListPlatform/wsgi.py
+++ ShoppingListPlatform/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ShoppingListPlatform.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ShoppingListPlatform.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
