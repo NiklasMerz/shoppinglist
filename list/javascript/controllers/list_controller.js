083084bffd4450baae63e7146868d5b88c44e21b
Niklas Merz <niklasmerz@linux.com>
2021-05-23 14:59:06 +0200

    Hide elements

--- list/javascript/controllers/list_controller.js
+++ list/javascript/controllers/list_controller.js
@@ -3,9 +3,23 @@ import { Controller } from "stimulus";
 
 // This file automatically initialised by stimulus-webpack-helper
 export default class extends Controller {
+    items = [];
 
     connect() {
-        console.log("Hello Stimulus from Controller List: ", this.element)
+        // Collect elements and hide elements without by flag
+        for (var i = 0; i < this.element.children.length; i++) {
+            let frame = this.element.children[i];
+            let item = frame.children[0];
+            
+            if (item.dataset.buy) {
+                this.items.push(item);
+            
+
+                if (item.dataset.buy === 'False') {
+                    item.hidden = true;
+                }
+            }
+        }
     }
 
 }
