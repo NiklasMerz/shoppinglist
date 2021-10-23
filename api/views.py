e06f7db58130cc265d463247c291f370386b0d91
Niklas Merz <niklasmerz@linux.com>
2021-10-23 23:51:47 +0200

    permissions for webui

--- api/views.py
+++ api/views.py
@@ -11,7 +11,7 @@ class ListViewSet(viewsets.ModelViewSet):
     """
     queryset = List.objects.all()
     serializer_class = ListSerializer
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
@@ -20,7 +20,7 @@ class ItemViewSet(viewsets.ModelViewSet):
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
 
 class StoreViewSet(viewsets.ModelViewSet):
     """
@@ -28,7 +28,7 @@ class StoreViewSet(viewsets.ModelViewSet):
     """
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
 
 class TripViewSet(viewsets.ModelViewSet):
     """
@@ -36,7 +36,7 @@ class TripViewSet(viewsets.ModelViewSet):
     """
     queryset = Trip.objects.all().order_by('-time')
     serializer_class = TripSerializer
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
 
 class CheckoutViewSet(viewsets.ModelViewSet):
     """
@@ -44,7 +44,7 @@ class CheckoutViewSet(viewsets.ModelViewSet):
     """
     queryset = Checkout.objects.all().order_by('-time')
     serializer_class = CheckoutSerializer
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         trip = Trip.objects.filter(id=self.request.data['trip']).first()
