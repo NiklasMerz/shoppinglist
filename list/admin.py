22bff8523e0f725c5ca1305cccd93b39d6def849
Niklas Merz <niklasmerz@linux.com>
2021-10-23 11:15:07 +0200

    expand model

--- list/admin.py
+++ list/admin.py
@@ -5,3 +5,6 @@ from .models import *
 
 admin.site.register(List)
 admin.site.register(Item)
+admin.site.register(Store)
+admin.site.register(Trip)
+admin.site.register(Checkout)
--- list/models.py
+++ list/models.py
@@ -4,12 +4,15 @@ from django.urls import reverse
 from turbo.mixins import BroadcastableMixin
 
 
+class Store(BroadcastableMixin, models.Model):
+    name = models.TextField()
+    note = models.TextField(default=None, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.TextField()
+
 class List(BroadcastableMixin, models.Model):
     name = models.CharField(max_length=255)
-
-    def get_absolute_url(self):
-        return reverse("detail", kwargs={"pk": self.pk})
-
+    list = models.ManyToManyField(Store, related_name="stores", null=True, blank=True)
 
 class Item(BroadcastableMixin, models.Model):
     broadcasts_to = ["list", "all-lists"]
@@ -24,3 +27,14 @@ class Item(BroadcastableMixin, models.Model):
     current_count = models.IntegerField(default=None, blank=True, null=True)
     last_count = models.IntegerField(default=None, blank=True, null=True)
     buy = models.BooleanField(default=True)
+class Trip(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    store = models.ForeignKey(Store, related_name="trips", on_delete=models.CASCADE)
+    list = models.ForeignKey(List, related_name="trips", on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+
+class Checkout(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    trip = models.ForeignKey(Trip, related_name="checkouts", on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name="checkouts", on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
