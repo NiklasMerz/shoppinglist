7de86a639d43fc4f808a98fcd6e6e77fecedcac7
Niklas Merz <niklasmerz@linux.com>
2021-11-11 18:18:41 +0100

    enter total for trip

--- webapp/src/app/pages/trip/trip.page.ts
+++ webapp/src/app/pages/trip/trip.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { ApiService, Trip } from 'src/app/backend';
 
 @Component({
@@ -11,12 +12,20 @@ export class TripPage implements OnInit {
   id: any;
   trip: Trip;
 
-  constructor(private api: ApiService, private activatedRouter: ActivatedRoute) { }
+  constructor(private api: ApiService, private activatedRouter: ActivatedRoute, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.loadTrips();
   }
 
+  save() {
+    this.api.updateTrip(this.trip.id.toString(), this.trip).subscribe(() => {
+      this.toastCtrl.create({
+        message: 'Trip updated',
+        duration: 2000
+      }).then(toast => toast.present());
+    });
+  }
 
   private async loadTrips() {
     this.id = this.activatedRouter.snapshot.params["trip"];
