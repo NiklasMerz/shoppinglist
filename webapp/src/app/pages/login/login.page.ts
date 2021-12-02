dec75c89b43975c3e7cb6190c83828ddc55608e0
Niklas Merz <niklasmerz@linux.com>
2021-12-02 22:47:02 +0100

    improve login form UX
    Closes Login fixes #30

--- webapp/src/app/pages/login/login.page.ts
+++ webapp/src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {OAuthService} from 'angular-oauth2-oidc';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,8 @@ export class LoginPage implements OnInit {
   public username: string;
   public password: string;
 
-  constructor(private router: Router, private authService: AuthService, private oauthService: OAuthService) { }
+  constructor(private router: Router, private authService: AuthService, private oauthService: OAuthService,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.oauthService.configure(this.authService.getConfig());
@@ -30,11 +32,18 @@ export class LoginPage implements OnInit {
   }
 
   async login() {
-    await this.authService.login(this.username, this.password);
+    try {
+      await this.authService.login(this.username, this.password);
 
-    // TODO modal or better?
-    await this.router.navigate(['/']);
-    location.reload();
+      // TODO modal or better?
+      await this.router.navigate(['/']);
+      location.reload();
+    } catch (e) {
+      this.alertCtrl.create({
+        message: 'Login failed',
+        buttons: ['OK']
+      }).then(toast => toast.present());
+    }
   }
 
   logout() {
