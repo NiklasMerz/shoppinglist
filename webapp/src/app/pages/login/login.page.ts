65b60dbdbf13bb2011f446e1481b2b89b6eb3c98
Niklas Merz <niklasmerz@linux.com>
2021-12-04 09:54:11 +0100

    Implement login loading
    See #30

--- webapp/src/app/pages/login/login.page.ts
+++ webapp/src/app/pages/login/login.page.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {OAuthService} from 'angular-oauth2-oidc';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +16,7 @@ export class LoginPage implements OnInit {
   public password: string;
 
   constructor(private router: Router, private authService: AuthService, private oauthService: OAuthService,
-    private alertCtrl: AlertController) { }
+    private alertCtrl: AlertController, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.oauthService.configure(this.authService.getConfig());
@@ -32,13 +32,19 @@ export class LoginPage implements OnInit {
   }
 
   async login() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
     try {
+      await loading.present();
       await this.authService.login(this.username, this.password);
+      loading.dismiss();
 
       // TODO modal or better?
       await this.router.navigate(['/']);
       location.reload();
     } catch (e) {
+      loading.dismiss();
       this.alertCtrl.create({
         message: 'Login failed',
         buttons: ['OK']
