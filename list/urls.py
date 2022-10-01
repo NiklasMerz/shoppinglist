5f11139536c230f98f0a835694ea99845f65b37e
Niklas Merz <niklasmerz@linux.com>
2022-10-01 22:58:01 +0200

    remove old hotwire pages

--- list/urls.py
+++ list/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from . import views
-
 urlpatterns = [
     path("", RedirectView.as_view(url='/admin', permanent=False)),
 ]
