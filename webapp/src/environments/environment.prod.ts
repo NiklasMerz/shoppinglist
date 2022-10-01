a520d2260d4d5854f9e20b4e561adc3e61193684
Niklas Merz <niklasmerz@linux.com>
2022-10-01 20:53:50 +0200

    new prod backend host

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE_PATH: 'https://shoppinglisthotwire.herokuapp.com',
+  API_BASE_PATH: 'https://shoppinglist.merzlabs.de',
   // For non openapi endpoints
   API_PREFIX: '/api',
   CLIENT_ID: '7efQCXuuih0jL9zwKwRRLT7aBrQXK5D7DQ0xthbF'
